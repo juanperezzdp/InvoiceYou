318539bdcef106cb9f5b5e17b01111d387ce0290
juan perez <juanperezzdp@gmail.com>
2023-08-21 15:29:10 -0500

    resposive all

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,25 @@
 import Navbar from "./components/Navbar";
-// import Empresa from "./components/Empresa";
-// import Client from "./components/Client";
-// import { useFloatingWindow } from "./Context/ContextComponents";
-// import Products from "./components/Products";
-// import Firm from "./components/Firm";
-// import Capture from "./components/Capture";
-// import ProductGuide from "./components/ProductGuide";
+import Empresa from "./components/Empresa";
+import Client from "./components/Client";
+import { useFloatingWindow } from "./Context/ContextComponents";
+import Products from "./components/Products";
+import Firm from "./components/Firm";
+import Capture from "./components/Capture";
+import ProductGuide from "./components/ProductGuide";
 import Invoice from "./components/Invoice";
 import { InvoiceProvider } from "./Context/InvoiceContext";
-// import Iva from "./components/Iva";
-// import Entidad from "./components/Entidad";
-//import Descripcion from "./components/Descripcion";
+import Iva from "./components/Iva";
+import Entidad from "./components/Entidad";
+import Descripcion from "./components/Descripcion";
 
 const App = () => {
-  //const { activeComponent } = useFloatingWindow();
+  const { activeComponent } = useFloatingWindow();
 
   return (
     <>
       <InvoiceProvider>
         <Navbar />
-        {/* {activeComponent === null && <ProductGuide />}
+        {activeComponent === null && <ProductGuide />}
         {activeComponent === "empresa" && <Empresa />}
         {activeComponent === "cliente" && <Client />}
         {activeComponent === "producto" && <Products />}
@@ -28,7 +28,7 @@ const App = () => {
         {activeComponent === "iva" && <Iva />}
         {activeComponent === "iva" && <Iva />}
         {activeComponent === "entidad" && <Entidad />}
-        {activeComponent === "descripcion" && <Descripcion />} */}
+        {activeComponent === "descripcion" && <Descripcion />}
 
         <Invoice />
       </InvoiceProvider>
