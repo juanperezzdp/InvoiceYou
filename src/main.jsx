38d5a8b8aec83b403d395f30dd54ea7a9fa20ec0
juan perez <juanperezzdp@gmail.com>
2023-11-19 19:25:00 -0500

    dalete

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,11 @@ import "./tailwind.css";
 import { FloatingProvider } from "./Context/ContextComponents";
 import { router } from "./routers/Routers";
 import { RouterProvider } from "react-router-dom";
-import { AuthProvider } from "./Context/authcontext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <FloatingProvider>
-      <AuthProvider>
-        <RouterProvider router={router} />
-      </AuthProvider>
+      <RouterProvider router={router} />
     </FloatingProvider>
   </React.StrictMode>
 );
